8514c6bf9206b930d229222cdabfd3c17ad34dc1
Diversiz <46844438+Diversiz@users.noreply.github.com>
2020-06-16 22:38:06 +0300

    01

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const btn = document.querySelector('button');
-const inp = document.querySelector('input');
-let text = document.querySelector('h1');
-let box = '';
+const btn_1 = document.querySelector('#btn_1');
+const text_input = document.querySelector('#text');
+let out = document.querySelector('#out');
 
-btn.onclick = ()=>{
-    text.innerHTML = inp.value;
+btn_1.onclick = ()=>{out.innerHTML = text_input.value;}
+
+const btn_2 = document.querySelector('#btn_2');
+const color_input = document.querySelector('#color');
+let out_2 = document.querySelector('#out_2');
+let box = document.querySelector('.box');
+
+btn_2.onclick = ()=>{
+    out_2.innerHTML = color_input.value;
+    out_2.style.color = color_input.value;
+    box.style.background = color_input.value;
+
+    console.log(color_input.value);
 }
+
+const btn_3 = document.querySelector('#btn_3');
+const date_input = document.querySelector('#date');
+let out_3 = document.querySelector('#out_3');
+
+btn_3.onclick = ()=>{out_3.innerHTML = date_input.value;}
